87f49d66fad05b4d4e77b807ed9b9617d8e94869
Jay Quigley <quigley.james.g@gmail.com>
2015-12-04 11:23:35 -0600

    wrap ajax call inside request.xhr? handler

--- app/controllers/search.rb
+++ app/controllers/search.rb
@@ -4,7 +4,10 @@ end
 
 post '/search' do
 	if request.xhr?
-		p params
-		params.to_json
+		# p params
+		yt = YouTubeSearch.new
+		qa = yt.search_query(params[:data],10)
+		# p qa
+		qa.to_json
 	end
 end

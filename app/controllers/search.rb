d11fa33fd32d99d4ccd92bdb924b1a89fc177125
Jay Quigley <quigley.james.g@gmail.com>
2015-12-14 11:59:06 -0600

    replace tabs with spaces

--- app/controllers/search.rb
+++ app/controllers/search.rb
@@ -1,11 +1,11 @@
 get '/' do
-	erb :"search/index"
+  erb :"search/index"
 end
 
 post '/search' do
-	# if request.xhr?
-	yt = YouTubeSearch.new
-	qa = yt.search_query(params[:data],10)
-	qa.to_json
-	# end
+  # if request.xhr?
+  yt = YouTubeSearch.new
+  qa = yt.search_query(params[:data],10)
+  qa.to_json
+  # end
 end
--- app/models/search/general_count_search.rb
+++ app/models/search/general_count_search.rb
@@ -5,36 +5,36 @@ require 'json'
 
 class YouTubeSearch
 
-	def search_query(query, how_many)
-		uri = URI.parse("https://www.youtube.com/results?search_query=#{query}")
+  def search_query(query, how_many)
+    uri = URI.parse("https://www.youtube.com/results?search_query=#{query}")
 
-		# doc is a Ruby object representing a whole html document
-		doc = Nokogiri::HTML(open(uri))
+    # doc is a Ruby object representing a whole html document
+    doc = Nokogiri::HTML(open(uri))
 
-		# find the first `how_many` divs wth the class '.yt-lockup-content'
-		div_objects = doc.css(".yt-lockup-content").take(how_many)
+    # find the first `how_many` divs wth the class '.yt-lockup-content'
+    div_objects = doc.css(".yt-lockup-content").take(how_many)
 
-		# map each div object to a hash for its title, url, and number of views
-		results = div_objects.map do |div|
+    # map each div object to a hash for its title, url, and number of views
+    results = div_objects.map do |div|
 
-			# select link (a) tag inside title yt-lockup-title div
-			link_objects = div.css(".yt-lockup-title").children.select do |child|
-			 	child.name == "a"
-			end
+      # select link (a) tag inside title yt-lockup-title div
+      link_objects = div.css(".yt-lockup-title").children.select do |child|
+         child.name == "a"
+      end
 
-			view_count = div.css(".yt-lockup-meta-info").children.last.text.gsub(/\D/,"").to_i
+      view_count = div.css(".yt-lockup-meta-info").children.last.text.gsub(/\D/,"").to_i
 
-			link_objects.map do |link|
-				{
-					title: link.text,
-					url: "https://www.youtube.com" + link.attributes["href"].value,
-					views: view_count
-				}
-			end
-		end.flatten
+      link_objects.map do |link|
+        {
+          title: link.text,
+          url: "https://www.youtube.com" + link.attributes["href"].value,
+          views: view_count
+        }
+      end
+    end.flatten
 
-		#convert the array of hashes to JSON
-		results.to_json
-	end
+    #convert the array of hashes to JSON
+    results.to_json
+  end
 
 end
